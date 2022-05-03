ef580c6bb0b99b7ec8e76bf16a563ed9eff1bd71
JunHong <alswnsghd1234@naver.com>
2022-05-03 10:43:04 +0900

    server

--- JSP_Project/WebContent/index.jsp
+++ JSP_Project/WebContent/index.jsp
@@ -7,21 +7,21 @@
 <title>Insert title here</title>
 </head>
 <body>
-      <!--      C(reate) R(ead) U(pdate) D(elete)  
-      회원 서비스  | insert /select / update / delete 
-      로그인        |        /   O   /        /        
-      회원가입      |   O    /       /        /           
-      내정보보기   |        /   O   /        /         
-      정보변경     |        /       /    O    /        
-      회원탈퇴     |        /       /         /    O 
-      
-      공지사항게시판 - 공지사항 리스트 조회(R)/공지사항 상세보기(R)/ 공지사항 작성(C) / 공지사항 수정(U) / 공지사항 삭제 (D)
-      일반게시판
-      사진게시판 
-      -->
-      
-      <h1>본격 프로젝트 진심모드</h1>
-      
-      <%com.kh.common.JDBCTemplate.getConnection();%> 
+		<!--      C(reate) R(ead) U(pdate) D(elete)  
+		회원 서비스  | insert /select / update / delete 
+		로그인        |        /   O   /        /        
+		회원가입      |   O    /       /        /           
+		내정보보기   |        /   O   /        /         
+		정보변경     |        /       /    O    /        
+		회원탈퇴     |        /       /         /    O 
+		
+		공지사항게시판 - 공지사항 리스트 조회(R)/공지사항 상세보기(R)/ 공지사항 작성(C) / 공지사항 수정(U) / 공지사항 삭제 (D)
+		일반게시판
+		사진게시판 
+		-->
+		<h1>본격 프로젝트 진심모드</h1>
+		
+		<%@ include file="views/common/menubar.jsp" %>
+		
 </body>
 </html>
--- JSP_Project/src/com/kh/common/JDBCTemplate.java
+++ JSP_Project/src/com/kh/common/JDBCTemplate.java
@@ -12,18 +12,19 @@ import java.util.Properties;
 
 public class JDBCTemplate {
 
-	//1.Connection 객체 생성 (DB 접속) 한 후 해당 Connection을 반환하는 메소드
-	
+	// 1.Connection 객체 생성 (DB 접속) 한 후 해당 Connection을 반환하는 메소드
+
 	public static Connection getConnection() {
-	
-		//Map계열 컬렉션 (key-value)
+
+		// Map계열 컬렉션 (key-value)
 		Properties prop = new Properties();
-		
-		//읽어들이고자 하는 driver.properties 파일의 물리적인 경로
-		//실질적으로 배포되는 폴더는 WebContent이기 때문에 src/db/driver/driver.properties로 읽으면 안됨
-		//WebContent/WEB-INF/classes에 있는 파일을 읽기때문
+
+		// 읽어들이고자 하는 driver.properties 파일의 물리적인 경로
+		// 실질적으로 배포되는 폴더는 WebContent이기 때문에 src/db/driver/driver.properties로 읽으면 안됨
+		// WebContent/WEB-INF/classes에 있는 파일을 읽기때문
 		String fileName = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
-		
+		// D:\server-workspace2\JSP_Project\WebContent\WEB-INF\classes\db\driver
+
 		try {
 			prop.load(new FileInputStream(fileName));
 		} catch (FileNotFoundException e1) {
@@ -33,18 +34,17 @@ public class JDBCTemplate {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		//1) jdbc driver 등록
-		
-		Connection conn =null;
+
+		// 1) jdbc driver 등록
+		Connection conn = null;
+
 		try {
 //			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Class.forName(prop.getProperty("driver"));
 //			conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SERVER","SERVER");
-			conn=DriverManager.getConnection(prop.getProperty("url"),
-													prop.getProperty("username"),
-													prop.getProperty("password"));
-			
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+					prop.getProperty("password"));
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -53,15 +53,15 @@ public class JDBCTemplate {
 			e.printStackTrace();
 		}
 		return conn;
+
 	}
-	
-	
-	
-	//2. 전달받은 Connection 객체를 가지고 commit 해주는 메소드
+
+	// 2.전달받은 Connection 객체를 가지고 commit 해주는 메소드
 	public static void commit(Connection conn) {
-		
+
 		try {
-			if(conn!=null&&!conn.isClosed()) {
+			if (conn != null && !conn.isClosed()) {
+
 				conn.commit();
 			}
 		} catch (SQLException e) {
@@ -69,11 +69,13 @@ public class JDBCTemplate {
 			e.printStackTrace();
 		}
 	}
-	//3. 전달받은 Connection 객체를 가지고 rollback 해주는 메소드
+
+	// 3.전달받은 Connection 객체를 가지고 rollback 해주는 메소드
 	public static void rollback(Connection conn) {
-		
+
 		try {
-			if(conn!=null&&!conn.isClosed()) {
+			if (conn != null && !conn.isClosed()) {
+
 				conn.rollback();
 			}
 		} catch (SQLException e) {
@@ -81,11 +83,13 @@ public class JDBCTemplate {
 			e.printStackTrace();
 		}
 	}
-	//4. 전달받은 Connection 객체를 반납시켜주는 메소드
+
+	// 4.전달받은 Connection 객체를 반납시켜주는 메소드
 	public static void close(Connection conn) {
-		
+
 		try {
-			if(conn!=null&&!conn.isClosed()) {
+			if (conn != null && !conn.isClosed()) {
+
 				conn.close();
 			}
 		} catch (SQLException e) {
@@ -93,27 +97,30 @@ public class JDBCTemplate {
 			e.printStackTrace();
 		}
 	}
-	//5. 전달받은 Statement 객체를 반납시켜주는 메소드
+
+	// 5.전달받은 Statement 객체를 반납시켜주는 메소드
 	public static void close(Statement stmt) {
-		
+
 		try {
-			if(stmt !=null&& !stmt.isClosed())
-			stmt.close();
+			if (stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	//6. 전달받은 ResultSet 객체를 반납시켜주는 메소드
-public static void close(ResultSet rset) {
-		
+	// 6.전달받은 ResultSet 객체를 반납시켜주는 메소드
+	public static void close(ResultSet rset) {
+
 		try {
-			if(rset !=null&& !rset.isClosed())
+			if (rset != null && !rset.isClosed()) {
 				rset.close();
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
